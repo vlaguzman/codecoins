a276b62ea51747c562911eb216bf3cb0d4d0f535
Vladimir Guzman <guzman.vla@gmail.com>
2020-05-25 19:28:51 -0500

    gist

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'game'
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', views.IndexView.as_view(), name='index'),
+  path('task/<int:task_id>/', views.detail, name='detail'),
+  path('task/<int:task_id>/subtasks/', views.subtasks, name='subtasks'),
+  path('task/<int:task_id>/join/', views.join, name='join'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from django.http import HttpResponse
+from .models import Task, SubTask
 
+class IndexView(generic.ListView):
+    template = 'game/index.html'
+    context_object_name = 'latest_task_list'
 
-def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+    def get_queryset(self):
+        return Task.objects.order_by('-assign_date')[:5]
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'game/detail.html', {'task': task})
+
+def subtasks(request, task_id):
+  response = "You're looking at the subtask of task %s."
+  return HttpResponse(response % task_id)
+
+def join(request, task_id):
+  return HttpResponse("You're ask for the task %s." % task_id)
